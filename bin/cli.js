e7a5eac6c05cf312da846ee5a899c859960fc342
Ryan Ramage <ryan.ramage@gmail.com>
2015-10-28 12:40:19 -0600

    add better logging, with resume from last change by reading the log file

--- bin/cli.js
+++ bin/cli.js
@@ -1,7 +1,13 @@
 #!/usr/bin/env node
 var path = require('path')
+var md5 = require('md5')
+var mkdirp = require('mkdirp')
+var bunyan = require('bunyan')
+var lastLine = require('last-line')
+
 var config = require('rc')('couch2elastic4sync', {
-  type: 'listing'
+  addRaw: false,
+  bunyan_base_path: '/tmp/couch2elastic4sync'
 })
 if (!config.elasticsearch) {
   console.log('No elasticsearch search.')
@@ -12,10 +18,54 @@ if (config.mapper && typeof config.mapper === 'string') {
   config.mapper = require(path.resolve(config.mapper))
 }
 
+var log = getLogFile(config)
+
 if (config._[0] === 'load') {
-  var load = require('../lib/load')(config.database, config.elasticsearch, config.mapper)
+  var load = require('../lib/load')(config.database, config.elasticsearch, config.mapper, config.addRaw, log)
   load.pipe(process.stdout)
 } else {
-  require('../lib')(config.database, config.elasticsearch, config.mapper)
+
+  getLastChange(config, function (err, since) {
+    if (err) {
+      console.log('an error occured', err)
+      console.log('using since = now')
+      since = null
+    }
+    else console.log('using since = ', since)
+    console.log('now logging to: ', getLogPath(config))
+    require('../lib')(config.database, config.elasticsearch, config.mapper, config.addRaw, log, since)
+  })
 }
 
+function getLogPath (config) {
+  var filename = md5(config.elasticsearch) + '.log'
+  return path.resolve(config.bunyan_base_path, filename)
+}
+
+function getLogFile (config) {
+  mkdirp.sync(config.bunyan_base_path)
+  var filename = md5(config.elasticsearch) + '.log'
+  var where = path.resolve(config.bunyan_base_path, filename)
+
+  var log = bunyan.createLogger({
+    name: 'couch2elastic4sync',
+    streams: [{
+        path: where
+    }]
+  })
+  return log
+}
+
+function getLastChange (config, cb) {
+  var logpath = getLogPath(config)
+  lastLine(logpath, function (err, res) {
+    if (err) return cb(err)
+
+    try {
+      var last_log = JSON.parse(res)
+      cb(null, last_log.change)
+
+    } catch(e) { cb(e) }
+
+  })
+}
