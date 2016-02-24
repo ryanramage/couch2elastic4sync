b13a614a04029299db143a2ae1affce728df89a5
Ryan Ramage <ryan.ramage@gmail.com>
2016-02-24 13:44:04 -0700

    store the seq number processes in elasticsearch.

--- bin/cli.js
+++ bin/cli.js
@@ -1,9 +1,12 @@
 #!/usr/bin/env node
 var path = require('path')
 var md5 = require('md5')
+var url = require('url')
 var mkdirp = require('mkdirp')
 var bunyan = require('bunyan')
 var lastLine = require('last-line')
+var jsonist = require('jsonist')
+var selectn = require('selectn')
 
 var config = require('rc')('couch2elastic4sync', {
   addRaw: false,
@@ -14,7 +17,8 @@ var config = require('rc')('couch2elastic4sync', {
   urlTemplate: false,
   load: {
     swallowErrors: false
-  }
+  },
+  checkpointSize: 20
 })
 if (!config.elasticsearch) {
   console.log('No elasticsearch search.')
@@ -24,6 +28,7 @@ if (!config.elasticsearch) {
 if (config.mapper && typeof config.mapper === 'string') {
   config.mapper = require(path.resolve(config.mapper))
 }
+config.seq_url = url.resolve(config.elasticsearch, '/' + url.parse(config.elasticsearch).pathname.split('/')[1] + '/_mapping/seq')
 
 var log = getLogFile(config)
 
@@ -39,7 +44,7 @@ if (config._[0] === 'load') {
     } else {
       console.log('since:', since)
     }
-    console.log('end_on_catchup:', config.end_on_catchup)
+    console.log('endOnCatchup:', config.endOnCatchup)
     console.log('logging to:', getLogPath(config))
     require('../lib')(config, log, since)
   })
@@ -82,9 +87,11 @@ function getLastChange (config, cb) {
 }
 
 function getSince (config, cb) {
-  if (config.since) {
-    cb(null, config.since)
-  } else {
-    getLastChange(config, cb)
-  }
+  if (config.since) return cb(null, config.since)
+  jsonist.get(config.seq_url, function (err, data) {
+    if (err) return cb(err)
+    var seq = selectn('idx-edm-v5.mappings.seq._meta.seq', data)
+    if (!seq) return cb('no seq number in elasticsearch at ' + config.seq_url)
+    return cb(null, seq)
+  })
 }
